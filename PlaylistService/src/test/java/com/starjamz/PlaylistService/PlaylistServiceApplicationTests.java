d66eba5d4670c85420ba95d0c76e32d87b76d32f
Louis <brxstarjam@gmail.com>
2026-02-23 11:30:35 -0500

    Adding the PlaylistService

---
+++ PlaylistService/src/test/java/com/starjamz/PlaylistService/PlaylistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starjamz.PlaylistService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
